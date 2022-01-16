9f691f1f6a3ae225190d79856e1d5f75c0ba6626
Caya de Haas <44554850+cayadehaas@users.noreply.github.com>
2022-01-16 20:38:49 +0100

    Add files via upload

---
+++ src/send_commands_neat.py
@@ -0,0 +1,97 @@
+from __future__ import print_function
+import os
+import neat
+import visualize
+
+import time
+import numpy as np
+
+import robobo
+import cv2
+import sys
+import signal
+import prey
+
+# 2-input XOR inputs and expected outputs.
+xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
+xor_outputs = [   (0.0,),     (1.0,),     (1.0,),     (0.0,)]
+
+### input is infrared sensors, output is movement of robot ###
+
+def terminate_program(signal_number, frame):
+    print("Ctrl-C received, terminating program")
+    sys.exit(1)
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = 4.0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        for xi, xo in zip(xor_inputs, xor_outputs):
+            output = net.activate(xi)
+            genome.fitness -= (output[0] - xo[0]) ** 2
+
+def run(config_file):
+    signal.signal(signal.SIGINT, terminate_program)
+
+    # rob = robobo.HardwareRobobo(camera=True).connect(address="192.168.1.7")
+    rob = robobo.SimulationRobobo().connect(address='192.168.68.114', port=19997)
+    # rob = robobo.SimulationRobobo().connect(address='localhost', port=19997)
+
+    rob.play_simulation()
+
+    # Following code moves the robot
+    # for i in range(2):
+    #        print("robobo is at {}".format(rob.position()))
+    #        rob.move(20, 20, 1000)
+
+    print("robobo is at {}".format(rob.position()))
+
+    # Following code gets an image from the camera
+    image = rob.get_image_front()
+    # IMPORTANT! `image` returned by the simulator is BGR, not RGB
+    cv2.imwrite("test_pictures.png", image)
+
+    time.sleep(0.1)
+
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
+
+    # Run for up to 300 generations.
+    winner = p.run(eval_genomes, 300)
+
+    # Display the winning genome.
+    print('\nBest genome:\n{!s}'.format(winner))
+
+    # Show output of the most fit genome against training data.
+    print('\nOutput:')
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    for xi, xo in zip(xor_inputs, xor_outputs):
+        output = winner_net.activate(xi)
+        print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
+
+    node_names = {-1:'A', -2: 'B', 0:'A XOR B'}
+    # visualize.draw_net(config, winner, True, node_names=node_names)
+    # visualize.plot_stats(stats, ylog=False, view=True)
+    # visualize.plot_species(stats, view=True)
+
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    p.run(eval_genomes, 10)
+
+if __name__ == '__main__':
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward')
+    run(config_path)
